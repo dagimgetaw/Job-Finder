780391e35cb4a58fe40eb2246d255a2bd50dce93
Dagim <dagimgetaW27@gmail.com>
2025-02-22 23:25:58 +0300

    add layout.jsx to the route

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-import Navbar from "./Components/Navbar/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Components/Home/Home";
+import Layout from "./Layout";
+import Login from "./Components/Login/Login";
+import Signup from "./Components/Signup/Signup";
 
 export default function App() {
   return (
-    <div>
-      <Navbar />
-      <Home />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          {/* <Route path="*" element={<NotFound />} /> */}
+        </Route>
+      </Routes>
+    </Router>
   );
 }
