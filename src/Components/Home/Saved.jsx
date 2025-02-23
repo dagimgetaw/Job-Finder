7c289f74a432de6dd8fa5fe82fc6982dc1abe394
Dagim <dagimgetaW27@gmail.com>
2025-02-23 17:13:21 +0300

    fix the overflow of the saved jobs

--- src/Components/Home/Saved.jsx
+++ src/Components/Home/Saved.jsx
@@ -11,16 +11,15 @@ export default function Saved({ jobs, handleBookMark, bookmarkedJobs }) {
     );
 
   return (
-    <div className="shadow-lg border border-gray-300 rounded-xl p-6 bg-white flex flex-col w-sm h-120 gap-4">
+    <div className="shadow-lg border border-gray-300 rounded-xl p-6 bg-white flex flex-col w-sm gap-4 max-h-[500px] overflow-y-auto relative">
       <h2 className="text-center text-xl font-semibold">Saved Jobs</h2>
-
       {savedJobs.length < 1 ? (
         <p className="text-center text-gray-500 py-4">No saved jobs</p>
       ) : (
         savedJobs.map((job) => (
           <div
             key={job.id}
-            className="border border-gray-300 rounded-lg p-4 bg-gray-50 flex flex-col gap-3"
+            className="border border-gray-300 rounded-lg p-4 bg-gray-50 flex flex-col gap-3 z-0"
           >
             <div className="flex justify-between items-start">
               <div>
