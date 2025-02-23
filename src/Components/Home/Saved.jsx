e85406b3125ff2524229cffa10efca958dc337e4
Dagim <dagimgetaW27@gmail.com>
2025-02-23 16:19:00 +0300

    reverse the saved jobs

---
+++ src/Components/Home/Saved.jsx
@@ -0,0 +1,51 @@
+import { X } from "lucide-react";
+
+// eslint-disable-next-line react/prop-types
+export default function Saved({ jobs, handleBookMark, bookmarkedJobs }) {
+  const savedJobs = jobs
+    // eslint-disable-next-line react/prop-types
+    .filter((job) => bookmarkedJobs.includes(job.id))
+    .sort(
+      // eslint-disable-next-line react/prop-types
+      (a, b) => bookmarkedJobs.indexOf(b.id) - bookmarkedJobs.indexOf(a.id)
+    );
+
+  return (
+    <div className="shadow-lg border border-gray-300 rounded-xl p-6 bg-white flex flex-col w-sm h-120 gap-4">
+      <h2 className="text-center text-xl font-semibold">Saved Jobs</h2>
+
+      {savedJobs.length < 1 ? (
+        <p className="text-center text-gray-500 py-4">No saved jobs</p>
+      ) : (
+        savedJobs.map((job) => (
+          <div
+            key={job.id}
+            className="border border-gray-300 rounded-lg p-4 bg-gray-50 flex flex-col gap-3"
+          >
+            <div className="flex justify-between items-start">
+              <div>
+                <h2 className="font-semibold text-lg">{job.title}</h2>
+                <p className="text-gray-600 text-sm">{job.company}</p>
+                <div className="flex gap-2 mt-2">
+                  <span className="px-3 py-1 rounded-lg bg-gray-200 text-xs">
+                    Remote
+                  </span>
+                  <span className="px-3 py-1 rounded-lg bg-gray-200 text-xs">
+                    {job.type}
+                  </span>
+                  <span className="px-3 py-1 rounded-lg bg-gray-200 text-xs">
+                    {job.salary}
+                  </span>
+                </div>
+              </div>
+              <X
+                className="hover:text-red-500 cursor-pointer transition-transform transform hover:scale-110"
+                onClick={() => handleBookMark(job.id)}
+              />
+            </div>
+          </div>
+        ))
+      )}
+    </div>
+  );
+}
