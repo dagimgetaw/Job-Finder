b11c0763c67a81010654e868959dd09ee819f0a5
Dagim <dagimgetaW27@gmail.com>
2025-02-24 14:24:51 +0300

    add JobDesc to the route

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./Components/Home/Home";
+import HomePage from "./Pages/HomePage";
 import Layout from "./Layout";
 import Login from "./Components/Login/Login";
 import Signup from "./Components/Signup/Signup";
+import JobDesc from "./Pages/JobDesc";
+import CreateJob from "./Pages/CreateJob";
 
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
+          <Route index element={<HomePage />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
+          <Route path="/job/:id" element={<JobDesc />} />>
+          <Route path="/create-job" element={<CreateJob />} />
           {/* <Route path="*" element={<NotFound />} /> */}
         </Route>
       </Routes>
